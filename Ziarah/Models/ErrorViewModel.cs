99774c598f81a8c5dc90db8e191ef8a818daf235
Amira1282004 <amiramahmoudgaber@gmail.com>
2026-04-14 20:58:26 +0200

    Add project files.

---
+++ Ziarah/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ziarah.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
