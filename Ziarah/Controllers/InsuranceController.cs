0a7edd00e0522b0aeb95c52416f4f49c0fda26c0
Saif El-Eslam <seiframadan125@gmail.com>
2026-04-21 04:19:57 +0200

    update

---
+++ Ziarah/Controllers/InsuranceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ziarah.Controllers
+{
+    public class InsuranceController : Controller
+    {
+        public IActionResult Insurance()
+        {
+            return View();
+        }
+    }
+}
---
+++ Ziarah/Controllers/MedicalTipsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ziarah.Controllers
+{
+    public class MedicalTipsController : Controller
+    {
+        public IActionResult MedicalTips()
+        {
+            return View();
+        }
+    }
+}
